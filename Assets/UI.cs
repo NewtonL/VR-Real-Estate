1075832adc95cecf67afed6061eb652ec45c813a
NewtonL <newton.lam@mail.utoronto.ca>
2016-12-29 12:34:48 -0800

    Add functionality to place objects using UI

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -5,25 +5,40 @@ using System.Collections;
 public class UI : MonoBehaviour {
 	
 	bool showUI = true;
+	GameObject newObj;
+	bool placing = false;
 
 	// Use this for initialization
 	void Start () {
-		this.transform.GetChild (0).gameObject.SetActive (false);
+		//this.transform.GetChild (0).gameObject.SetActive (false);
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("escape")) {
+
+		this.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 30;	//keep menu in front of camera view
+
+		if (Input.GetKeyDown ("escape")) {	//use ESC key to hide or show menu
 			this.transform.GetChild (0).gameObject.SetActive (showUI);
 			showUI = !showUI;
+		}
+
+		if (placing) {
+			newObj.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 10;
 		}
 	}
 
 	public void Button1(){
-		Vector3 v = new Vector3 (0, 0, 0);
-		Quaternion q = new Quaternion (0, 0, 0, 0);
-		Instantiate (Resources.Load("Wall"), v, q);
+		if (placing == false) {
+			Vector3 v = Camera.main.transform.position + Camera.main.transform.forward * 10;	//position in front of the camera's view
+			Quaternion q = new Quaternion (0, 0, 0, 0);
+			newObj = (GameObject)Instantiate (Resources.Load ("Wall"), v, q);
+			placing = true;
+		} else if (placing == true) {
+			placing = false;
+		}
 	}
 
 	public void Button2(){
