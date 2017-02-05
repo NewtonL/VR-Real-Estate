f31a94c11d02b21a5b30a16227c5852563587138
Newton Lam <newton.lam@mail.utoronto.ca>
2017-02-04 22:29:00 -0500

    Fixed menu button selection

---
+++ Assets/IgnoreMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IgnoreMouse : MonoBehaviour {
+
+	GameObject selected;
+
+	// Use this for initialization
+	void Start () {
+		selected = EventSystem.current.currentSelectedGameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (EventSystem.current.currentSelectedGameObject == null) {
+			EventSystem.current.SetSelectedGameObject (selected);
+		}
+		selected = EventSystem.current.currentSelectedGameObject;
+	}
+}
