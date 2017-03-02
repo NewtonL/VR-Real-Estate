59583b6da91ddf58a0e6fb7b0df2ea90cbed11b6
NewtonL <newton.lam@mail.utoronto.ca>
2017-03-02 13:46:39 -0500

    Added touchpad support for Welcome UI navigation

--- Assets/IgnoreMouse.cs
+++ Assets/IgnoreMouse.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class IgnoreMouse : MonoBehaviour {
 
@@ -10,6 +11,33 @@ public class IgnoreMouse : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		selected = EventSystem.current.currentSelectedGameObject;
+		OVRTouchpad.Create ();
+		OVRTouchpad.TouchHandler += HandleTouchHandler;
+	}
+
+	void HandleTouchHandler(object sender, System.EventArgs e){
+		Button b = selected.GetComponent<Button> ();
+		OVRTouchpad.TouchArgs touch = (OVRTouchpad.TouchArgs)e;
+		if (touch.TouchType == OVRTouchpad.TouchEvent.Left) {
+			EventSystem.current.SetSelectedGameObject (b.FindSelectableOnLeft().gameObject);
+			print ("Left");
+		}
+		if (touch.TouchType == OVRTouchpad.TouchEvent.Right) {
+			EventSystem.current.SetSelectedGameObject (b.FindSelectableOnRight().gameObject);
+			print ("Right");
+		}
+		if (touch.TouchType == OVRTouchpad.TouchEvent.Up) {
+			EventSystem.current.SetSelectedGameObject (b.FindSelectableOnUp().gameObject);
+			print ("Up");
+		}
+		if (touch.TouchType == OVRTouchpad.TouchEvent.Down) {
+			EventSystem.current.SetSelectedGameObject (b.FindSelectableOnDown().gameObject);
+			print ("Down");
+		}
+		if (touch.TouchType == OVRTouchpad.TouchEvent.SingleTap) {
+			b.onClick.Invoke ();
+			print ("Tap");
+		}
 	}
 	
 	// Update is called once per frame
