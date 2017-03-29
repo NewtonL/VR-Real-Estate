be445545c2edca5af2d888bf378c5ac92a4699bf
Newton Lam <newton.lam@mail.utoronto.ca>
2017-03-28 20:53:45 -0400

    Add door to object selection

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -118,6 +118,16 @@ public class UI : MonoBehaviour {
 
 			placing = true;
 		}
+		else if (s.Equals("door")) {
+			Vector3 v = Camera.main.transform.position + Camera.main.transform.forward * 10;	//position in front of the camera's view
+			newObj = (GameObject)Instantiate (Resources.Load (s), v, Quaternion.Euler (0, 270, 0));
+
+			RaycastHit hitInfo;
+			Physics.Raycast (newObj.transform.position, -Vector3.up, out hitInfo);
+			distanceFromGround = hitInfo.distance;
+
+			placing = true;
+		}
 	}
 
 	public void ChangeWallColour(){
